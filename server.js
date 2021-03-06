6090131178bfd867a1f10fa011cf3ad25139426d
Matthew Cook <matthewcook13987@gmail.com>
2021-03-06 21:57:44 +0000

    Added a new route handler for api/items to display the products in JSON format.

--- server.js
+++ server.js
@@ -35,10 +35,12 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const itemsRoutes = require("./routes/items");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
+app.use("/api/items", itemsRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
