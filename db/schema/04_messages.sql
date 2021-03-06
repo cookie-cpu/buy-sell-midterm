cd4b01d688facf0e3b7db496c01ebb1842031f9f
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-06 19:18:22 +0000

    modify msgs and favs schemas

---
+++ db/schema/04_messages.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE messages (
+ sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+ recipient_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+ message TEXT NOT NULL
+);
