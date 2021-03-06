9473183a154a57b8b52ec1adf15f6391dcac5039
Matthew Cook <matthewcook13987@gmail.com>
2021-03-06 22:18:08 +0000

    Added a basic nav tag to index.html with some basic styling.

--- routes/items.js
+++ routes/items.js
@@ -12,8 +12,8 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM items;`)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const items = data.rows;
+        res.json({ items });
       })
       .catch(err => {
         res
