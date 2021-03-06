427e266799aa31c01a3f807e62650d01b4806e7c
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-06 20:46:15 +0000

    fixed syntax error

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -3,5 +3,5 @@ VALUES ('daisies', 'bright and cheerful', 5, 'photo_url', FALSE),
 ('forget-me-nots', 'small, dainty, purple', 7, 'photo_url', FALSE),
 ('lilac', 'purple fragrant', 20, 'photo_url', FALSE),
 ('lillies','they are very nice', 10, 'photo_url', FALSE),
-('roses','they are very red' 15, 'photo_url', FALSE),
-('marigold','vibrant orange color' 10, 'photo_url', FALSE);
+('roses','they are very red', 15, 'photo_url', FALSE),
+('marigold','vibrant orange color', 10, 'photo_url', FALSE);
