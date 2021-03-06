20cb36227b147c9fa23dad75a4fe2f936741f402
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-06 18:50:45 +0000

    add users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, admin)
+VALUES ('Alice', 'Alice@email.com', '123', FALSE),
+ ('Bob', 'Bob@email.com', '123', FALSE),
+ ('Joe', 'Joe@email.com', '123', FALSE),
+ ('Kira', 'Kira@nomail.com', '123', TRUE),
+ ('Emma', 'Emma@nomail.com', '123', TRUE);
+
+
