6e2ee08fecd7492fa0e0d58be791b6ad81fd8f04
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-07 22:13:15 +0000

    add items routes for / and /:id

--- routes/items.js
+++ routes/items.js
@@ -9,17 +9,41 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM items;`)
       .then(data => {
-        const items = data.rows;
-        res.json({ items });
+        console.log('the data is: ', data.rows)
+        const items = data.rows[0];
+        res.render("items", {items});
+        //res.json({ items });
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
-  });
-  return router;
+    });
+    return router;
+};
+
+module.exports = (db) => {
+
+  router.get("/:id", (req, res) => {
+    console.log('req.params.id is:', req.params.id);
+    db.query(`SELECT * FROM items WHERE id = $1`,[req.params.id])
+      .then(data => {
+        console.log('the data is: ', data.rows[0])
+        const items = data.rows[0];
+        res.render("items", {items});
+        //res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+    });
+    return router;
 };
