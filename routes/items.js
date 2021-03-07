536b66d913667bcb76a4dee5fcaca58178d1d726
Anita Toth <haluska99@hotmail.com>
2021-03-07 23:28:52 +0000

    set up

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,5 @@
 /*
- * All routes for Users are defined here
+ * All routes for items are defined here
  * Since this file is loaded in server.js into api/users,
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
