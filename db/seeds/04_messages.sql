914a24a21136fc1a9dd4515dc9e1c85f5e712958
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-08 23:16:43 +0000

    add more data to table

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,3 +1,5 @@
 INSERT INTO messages (sender_id, recipient_id, message)
-VALUES (1, 4, 'I like your flowers'),
-(2, 4, 'Will you negotiate on the price?');
+VALUES (1, 4, 'I like your flowers.'),
+(2, 4, 'Will you negotiate on the price?'),
+(3, 5, 'Will you offer a discount?'),
+(2, 5, 'Do they come in any different colours?');
