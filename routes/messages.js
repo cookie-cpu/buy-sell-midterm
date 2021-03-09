d7d04953b6584220f65d937333dbe6f618d097bb
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-09 20:33:06 +0000

    fix errors with post route

--- routes/messages.js
+++ routes/messages.js
@@ -14,7 +14,7 @@ module.exports = (db) => {
         console.log('the get / data is: ', data.rows)
         console.log('session', req.session.user_id)
         const messages = data.rows;
-        res.render('messages', {messages, userID:req.session.user_id});
+        res.render('messages', {messages, userID:req.session.user_id});  //add recipient name via jOIN with id
       })
       .catch(err => {
         res
@@ -31,7 +31,7 @@ module.exports = (db) => {
       FROM messages
       WHERE sender_id = $1
       AND recipient_id = $2
-      ORDER BY timestamp DESC`
+      ORDER BY timestamp`
       ,[req.session.user_id, req.params.id])
       .then(data => {
         console.log('the get /:id data is: ', data.rows[0])
@@ -47,19 +47,19 @@ module.exports = (db) => {
   });
 
   // ADD - message
-  router.post('/', (req, res) => {
-    // router.post('/:id', (req, res) => {   // tried change route to /:id but broke route. how to incorporate req.params.id?
-    console.log('req.body is:', req.body)
+  // router.post('/', (req, res) => {
+  router.post('/:id', (req, res) => {   // tried change route to /:id but broke route. how to incorporate req.params.id?
+    console.log('req.body.recipient is:', req.body.recipient_id)
     console.log('req.params.id is ', req.params.id)
     db.query(`
     INSERT INTO messages (sender_id, recipient_id, message)
      VALUES ($1, $2, $3) RETURNING *;`,
-    [req.body.message])  // req.body.sender_id, req.body.recipient_id, aren't being passed thru. removed for now. tried adding req.params.id, but didn't work
+    [req.session.user_id, req.params.id, req.body.message])  // req.body.sender_id, req.session.user_id, aren't being passed thru. removed for now. tried adding req.params.id, but didn't work
       .then(data => {
         console.log('the post / data is: ', data.rows[0])
         const message = data.rows[0];  // not being called. delete?
-        res.redirect('/messages');
-        // res.redirect('/messages/:id'); // change to messages/:id ? so it returns to the specific msg history
+        // res.redirect('/messages');
+        res.redirect(`/messages/${req.params.id}`); // change to messages/:id ? so it returns to the specific msg history
       })
       .catch(err => {
         res
