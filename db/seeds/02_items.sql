3eb509c18dc517e1d88ed2eafbcfbf9705b4964f
Anita Toth <haluska99@hotmail.com>
2021-03-09 22:43:22 +0000

    added description to items seeds and changed some one the messages in messages seeds

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,7 +1,7 @@
 INSERT INTO items (user_id, name, description, price, photo_url, sold)
-VALUES (4, 'The Loopy', 'bright and cheerful', 999, 'https://i.imgur.com/cnpWUCe.jpg', FALSE),
-(4, 'The Eye', 'small, dainty, purple', 555, 'https://i.imgur.com/Bji47Hr.jpg', FALSE),
-(5, 'The Tadpole', 'purple fragrant', 222, 'https://i.imgur.com/P3gJqD6.png', FALSE),
-(5, 'The Sombrero','they are very nice', 3333, 'https://i.imgur.com/XE7i9Q2.jpg', FALSE),
-(5, 'The Cigar','they are very red', 4321, 'https://i.imgur.com/xzZz3Cl.jpg', FALSE),
-(5, 'The Whirlpool','vibrant orange color', 123, 'https://i.imgur.com/7n9wQZI.jpg', FALSE);
+VALUES (4, 'The Loopy', 'For a good time 100 million light-years from Earth', 999, 'https://i.imgur.com/cnpWUCe.jpg', FALSE),
+(4, 'The Eye', 'If you like that feeling of being watched...', 555, 'https://i.imgur.com/Bji47Hr.jpg', FALSE),
+(5, 'The Tadpole', 'Perfect for amphibian enthusiasts!', 222, 'https://i.imgur.com/P3gJqD6.png', FALSE),
+(5, 'The Sombrero','Taco Tuesday everyday!', 3333, 'https://i.imgur.com/XE7i9Q2.jpg', FALSE),
+(5, 'The Cigar','Non-smokers only.', 4321, 'https://i.imgur.com/xzZz3Cl.jpg', FALSE),
+(5, 'The Whirlpool','For fans of Whirlpool appliances.', 123, 'https://i.imgur.com/7n9wQZI.jpg', FALSE);
--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,5 +1,5 @@
 INSERT INTO messages (sender_id, recipient_id, message)
-VALUES (1, 4, 'I like your flowers.'),
+VALUES (1, 4, 'Are there any UFOs?'),
 (2, 4, 'Will you negotiate on the price?'),
 (3, 5, 'Will you offer a discount?'),
-(2, 5, 'Do they come in any different colours?');
+(2, 5, 'How far aways is it from Earth?');
