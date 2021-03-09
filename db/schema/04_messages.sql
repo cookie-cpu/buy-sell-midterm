ce8f4ee4b17f75c3ffe5f8d42c1104f48e8b4cf4
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-09 00:47:58 +0000

    add data

--- db/schema/04_messages.sql
+++ db/schema/04_messages.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS messages CASCADE;
 CREATE TABLE messages (
  sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
  recipient_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
- message TEXT NOT NULL
+ message TEXT NOT NULL,
+ timestamp TIMESTAMP DEFAULT NOW()::timestamp
 );
