f792027f820d4450aba8a6bc94f1aaf826e08c8a
Matthew Cook <matthewcook13987@gmail.com>
2021-03-10 23:09:54 +0000

    Added more seed data for items and adjusted featured page helper function to match the new max number of post ids

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,7 +1,11 @@
 INSERT INTO items (user_id, name, description, price, photo_url, sold)
-VALUES (4, 'The Loopy', 'For a good time 100 million light-years from Earth', 999, 'https://i.imgur.com/cnpWUCe.jpg', FALSE),
+VALUES
+(4, 'The Loopy', 'For a good time 100 million light-years from Earth', 999, 'https://i.imgur.com/cnpWUCe.jpg', FALSE),
 (4, 'The Eye', 'If you like that feeling of being watched...', 555, 'https://i.imgur.com/Bji47Hr.jpg', FALSE),
 (5, 'The Tadpole', 'Perfect for amphibian enthusiasts!', 222, 'https://i.imgur.com/P3gJqD6.png', FALSE),
 (5, 'The Sombrero','Taco Tuesday everyday!', 3333, 'https://i.imgur.com/XE7i9Q2.jpg', FALSE),
 (5, 'The Cigar','Non-smokers only.', 4321, 'https://i.imgur.com/xzZz3Cl.jpg', FALSE),
-(5, 'The Whirlpool','For fans of Whirlpool appliances.', 123, 'https://i.imgur.com/7n9wQZI.jpg', FALSE);
+(5, 'The Whirlpool','For fans of Whirlpool appliances.', 123, 'https://i.imgur.com/7n9wQZI.jpg', FALSE),
+(5, 'The Milky Way','Not safe for lactose intolerant beings', 523, 'https://cdn.mos.cms.futurecdn.net/D48pgYXHy6yoawqH8URXLS-970-80.jpg.webp', FALSE),
+(5, 'The Cartwheel','Lots of space for gymnastics!', 891, 'https://cdn.mos.cms.futurecdn.net/9GbFAamZhbNngRkoR5kVrJ-970-80.jpg.webp', FALSE),
+(4, 'The Sunflower','Sunflowers not provided', 557, 'https://cdn.mos.cms.futurecdn.net/p6srozXjHT6hZJdobr76ge-970-80.jpg.webp', FALSE);
