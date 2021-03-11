2f8e1f007039fb93a666363e4c16c70f329a5e56
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-11 23:27:42 +0000

    remove console.logs, empty space and eslinted

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,13 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
 
   // BROWSE - view all items listed by a specific user ==> GET  /items/:user_id
-
   router.get("/:user_id", (req, res) => {
-    const userID = parseInt(req.session.user_id, 10)
-    console.log('userid', userID)
+
+    const userID = parseInt(req.session.user_id, 10);
+
     db.query(`
     SELECT items.*, users.name as username
     FROM items
@@ -22,14 +15,8 @@ module.exports = (db) => {
     WHERE user_id = $1;
     `, [userID])
       .then(data => {
-        // console.log('data on line 23', data)
         const items = data.rows;
-        const username = data.rows[0].username;
-        console.log('items', items)
-        console.log('userID', userID)
-        console.log('userName', username)
-        res.render('items', {items: items, userID, username });
-        // res.json({ users });
+        res.render('items', {items: items, userID});
       })
       .catch(err => {
         res
